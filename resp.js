88bf268e2b567f4be66ff579d5636d28cc13c13d
DivyaShukla21 <70100051+DivyaShukla21@users.noreply.github.com>
2021-10-09 10:06:27 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.buger')
+navbar = document.querySelector('.navbar')
+burger = document.querySelector('.navlist')
+
+
+
+burger.addEventListener('click'. ()=>{
+    navlist.classList.toggle('v-class'),
+    navbar.classList.toggle('h-nav'),
+
+})
